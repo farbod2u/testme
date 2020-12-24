c5deb2f0e971c9a15e3b5c9caf112f2aff1e9686
farbod2u <saeedsaf@gmail.com>
2020-12-24 12:42:13 +0330

    اضافه کردن به

---
+++ first/src/main/java/first/SaeedMain.java
@@ -0,0 +1,9 @@
+package first;
+
+public class SaeedMain {
+
+	public static void main(String[] args) {
+		System.out.println("\t\tHello World.");
+	}
+
+}
---
+++ first/src/test/java/first/SaeedMavenTest.java
@@ -0,0 +1,12 @@
+package first;
+
+import org.testng.annotations.Test;
+
+public class SaeedMavenTest {
+
+	@Test
+	public void testIt()
+	{
+		System.out.println("\t........ Saeed OK .........");
+	}
+}
