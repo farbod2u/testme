de40512cfb48027a13bcab878efb6c200b731dc7
farbod2u <saeedsaf@gmail.com>
2021-01-04 18:55:41 +0330

    xfvxcv

---
+++ student.class/src/main/java/saeed/hibernate/entities/Cource.java
@@ -0,0 +1,12 @@
+package saeed.hibernate.entities;
+
+import java.io.Serializable;
+
+public class Cource implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1472547662590449962L;
+
+}
