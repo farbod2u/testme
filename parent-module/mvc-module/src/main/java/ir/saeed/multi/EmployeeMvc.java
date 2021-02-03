c36c704c19a94b100ba2d11a9c95ba04babc98b5
saeed <saeed@saeed-vm.localdomain>
2021-02-03 10:33:00 +0000

    1399-11-15

---
+++ parent-module/mvc-module/src/main/java/ir/saeed/multi/EmployeeMvc.java
@@ -0,0 +1,20 @@
+package ir.saeed.multi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeMvc {
+	
+	@Autowired
+	EmployeeService service;
+	
+	@GetMapping("/getall")
+	public String getAll(Model model)
+	{
+		model.addAttribute("data", service.getAll());
+		return "listAll";
+	}
+}
